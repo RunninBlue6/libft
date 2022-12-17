05e6eddbbc3c9cad3bcd7b9eb94f6d219618ffb8
RunninBlue6 <105640634+RunninBlue6@users.noreply.github.com>
2022-12-17 23:40:05 +0100

    ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jknyzhen < jknyzhen@student.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/17 23:33:48 by jknyzhen          #+#    #+#             */
+/*   Updated: 2022/12/17 23:33:48 by jknyzhen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*The memmove() function copies n bytes from memory area src to
+       memory area dest.  The memory areas may overlap: copying takes
+       place as though the bytes in src are first copied into a
+       temporary array that does not overlap src or dest, and the bytes
+       are then copied from the temporary array to dest.
+       The memmove() function returns a pointer to dest.*/
