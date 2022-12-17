b668239617fdcbe3fc84ce043bcd4844fd732f7f
RunninBlue6 <105640634+RunninBlue6@users.noreply.github.com>
2022-12-17 20:08:49 +0100

    ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -15,6 +15,8 @@
 int ft_isalnum(int c)
     {
         if ((c>= 'A' && c<= 'Z') || (c>= 'a' && c<= 'z')) || (c>= '0' && c<= '9')
-            return(1);
-        return(0);
+            return  (1);
+        else
+            return  (0);
     }
+    
