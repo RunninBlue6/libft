b63ddf9d65dd94ace613edb06ca13313623f2de3
RunninBlue6 <105640634+RunninBlue6@users.noreply.github.com>
2022-12-17 20:12:16 +0100

    ft_isascii.c

--- ft_isascii.c
+++ ft_isascii.c
@@ -15,6 +15,8 @@
 int ft_ascii(int c)
     {
         if  (c >= 0 && c <= 127)
-            return(1);
-        return(0);
+            return  (1);
+        else
+            return  (0);
     }
+    
