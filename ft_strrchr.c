bc136104c0994e6adbd641ca031b96c3716228d2
Juliana Knyzhenko <jknyzhen@c2s8d31.42wolfsburg.de>
2022-12-18 13:53:08 +0100

    ft_strrchr.c

---
+++ ft_strrchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/18 13:46:11 by jknyzhen          #+#    #+#             */
+/*   Updated: 2022/12/18 13:50:38 by jknyzhen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//The strrchr() function returns a pointer to the LAST occurrence of the character c in the string s.
+
+char *ft_strrchr(const char *s, int c)
+{
+    char *p;
+
+    p = NULL;
+    while (*s != '\0')
+    {
+        if (*s == c)
+            {
+                p = (char *)s;
+            }
+        s++;
+    }
+    if (c == '\0')
+    {
+        return  ((char *)s);
+    }
+    return  (p);
+}
