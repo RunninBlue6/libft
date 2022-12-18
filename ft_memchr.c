4f0b77d3cf5b0d3a330c41259f233098cd022e78
Juliana Knyzhenko <jknyzhen@c2s8d31.42wolfsburg.de>
2022-12-18 15:38:55 +0100

    ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/18 15:19:14 by jknyzhen          #+#    #+#             */
+/*   Updated: 2022/12/18 15:36:29 by jknyzhen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*The memchr() function scans the initial n bytes of the memory
+       area pointed to by s for the first instance of c.  Both c and the
+       bytes of the memory area pointed to by s are interpreted as
+       unsigned char.*/
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    size_t  i;
+
+    unsigned char   *p;
+    p = (unsigned char *)s;
+
+    i = 0;
+    while   (i < n)
+    {
+        if  (*p == (unsigned char )c)
+        {
+            return  ((void *)p);
+        }
+        p++;
+        i++;
+    }
+    return  (NULL);
+}
