fd8ea44b92fa40630cb07a8387041d7087e3fa36
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-22 16:11:44 +0100

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,29 +6,29 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 12:26:22 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/19 17:00:25 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2022/12/22 13:59:04 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+// #include "libft.h"
 
-size_t  *ft_strlcat(char *dest, char *src)
-{
-    int i;
-    int d_s;
+// size_t  *ft_strlcat(char *dest, char *src)
+// {
+//     int i;
+//     int d_s;
 
-    int = 0;
-    d_s = 0;
-    while (dest[d_s])
-    {
-        d_s++;
-    }
-    while   (src[i])
-    {
-        dest[d_s] = src[i];
-        d_s++;
-        i++;
-    }
-    dest[d_s] = '\0';
-    return  (dest);
-}
+//     int = 0;
+//     d_s = 0;
+//     while (dest[d_s])
+//     {
+//         d_s++;
+//     }
+//     while   (src[i])
+//     {
+//         dest[d_s] = src[i];
+//         d_s++;
+//         i++;
+//     }
+//     dest[d_s] = '\0';
+//     return  (dest);
+// }
