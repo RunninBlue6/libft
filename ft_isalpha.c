fafdf77ea0aa59c6a8845c0db454089ca9266eda
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-22 16:09:16 +0100

    ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 11:44:42 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/19 17:03:29 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2022/12/22 13:58:42 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,19 @@ int ft_isalpha(int c)
     else
 		return (0);
     }
+
+// int main()
+// {
+//     char c;
+//     c = 'Q';
+//     printf("\nResult when uppercase alphabet is passed: %d", isalpha(c));
+
+//     c = 'q';
+//     printf("\nResult when lowercase alphabet is passed: %d", isalpha(c));
+
+//     c='+';
+//     printf("\nResult when non-alphabetic character is passed: %d", isalpha(c));
+
+//     return 0;
+// }
+
