f1b829007b3808e0d6d8eb24da5ee1b1117d8824
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-23 14:27:26 +0100

    ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,22 +6,26 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 21:13:27 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/19 16:59:02 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2022/12/23 14:11:53 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
 #include "libft.h"
-
-char    *ft_strlcpy(char *a, char *b) //a-destenation b-source
+/*a-destenation b-source*/
+char	*ft_strlcpy(char *a, char *b)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (b[i] != '\0')
-    {
-        a[i] = b[i];
-        i++;
-    }
-    a[i] = '\0';
-    return (a);
+	i = 0;
+	while (b[i] != '\0')
+	{
+		a[i] = b[i];
+		i++;
+	}
+	a[i] = '\0';
+	return (a);
 }
