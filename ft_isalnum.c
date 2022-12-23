1d0a6835c4e04d2d6070285cca8bec2baf6cfe04
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-23 13:40:54 +0100

    ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -5,18 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/16 22:02:40 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/19 17:00:02 by jknyzhen         ###   ########.fr       */
+/*   Created: 2022/12/23 13:39:02 by jknyzhen          #+#    #+#             */
+/*   Updated: 2022/12/23 13:40:15 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isalnum(int c)
+int	ft_isalnum(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+	if ((c >= 'A' && c <= 'Z')
+		|| (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
 		return (1);
 	else
 		return (0);
 }
-    
