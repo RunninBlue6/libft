9e3278de7f548311f4ad7aad8d59a1a1489b09df
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-23 13:55:59 +0100

    ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 16:30:18 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/22 17:42:53 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2022/12/23 13:55:32 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <limits.h>
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	int	i;
 	int	convert;
@@ -25,7 +25,6 @@ int		ft_atoi(const char *str)
 	i = 0;
 	convert = 1;
 	result = 0;
-
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 	{
 		i++;
@@ -36,7 +35,7 @@ int		ft_atoi(const char *str)
 		convert = -1;
 		i++;
 	}
-	while (str[i] && str[i] >= 48 && str[i] <=57)
+	while (str[i] && str[i] >= 48 && str[i] <= 57)
 	{
 		result = (result * 10) + str[i] - 48;
 		i++;
