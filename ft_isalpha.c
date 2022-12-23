fdbca14af0549faa9f7277c47af5b8ae125b15c6
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-23 13:01:11 +0100

    ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,19 +6,21 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 11:44:42 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/22 13:58:42 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2022/12/23 13:00:41 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z'))
+	{
 		return (1);
-    else
-		return (0);
-    }
+	}
+	return (0);
+}
 
 // int main()
 // {
@@ -30,8 +32,7 @@ int ft_isalpha(int c)
 //     printf("\nResult when lowercase alphabet is passed: %d", isalpha(c));
 
 //     c='+';
-//     printf("\nResult when non-alphabetic character is passed: %d", isalpha(c));
+//     printf("\nnon-alphabetic character is passed: %d", isalpha(c));
 
 //     return 0;
 // }
-
