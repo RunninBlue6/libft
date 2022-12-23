61dc5b0b2ce4ec0d74d7fb2007e6fa926fa5f441
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-23 15:06:25 +0100

    ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,21 +6,32 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 21:12:51 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/19 16:59:56 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2022/12/23 15:06:01 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
 #include "libft.h"
 
-void    *ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *s, int c, size_t n)
 {
-    unsigned char    *p;
+	unsigned char		*ptr;
+	unsigned char		value;
+	unsigned long int	i;
 
-    p = (unsigned char *)b;
-    while (len > 0)
-    {
-        p[len-1] = c;
-        len--;
-    }
-    return (b);
+	if (n > 0)
+	{
+		i = 0;
+		ptr = (unsigned char *) s;
+		value = (unsigned char) c;
+		while (i < n)
+		{
+			ptr[i] = value;
+			i++;
+		}
+	}
+	return (s);
 }
