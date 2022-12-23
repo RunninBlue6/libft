184110af0f1889aa195b1160d4204646695aeddb
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-23 16:29:51 +0100

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 16:25:46 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/22 17:37:05 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2022/12/23 15:05:37 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_ascii(int c);
 int		ft_isprint(int c);
-int		ft_strlen(char  *str);
-void    *ft_memset(void *b, int c, size_t len);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
-void   	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t len);
 char	*ft_strlcpy(char *a, char *b);
-size_t 	*ft_strlcat(char *dest, char *src);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
@@ -36,5 +36,4 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
 //strnstr
-//atoi
 #endif
