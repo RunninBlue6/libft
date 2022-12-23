3b9a0ff29dd811f98cb0be952a8e0b4bc4330a4f
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-23 15:08:18 +0100

    ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,27 +6,31 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 13:08:24 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/19 16:59:57 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2022/12/23 15:07:43 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//The strchr() function returns a pointer to the first occurrence of the character c in the string s.
+//The strchr() function returns a pointer to the 
+//first occurrence of the character c in the string s.
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
 #include "libft.h"
 
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-    while   (*s != '\0')
-    {
-        if ((unsigned char )*s == (unsigned char)c)
-        {
-            return ((char *)s);
-        }
-        s++;
-    }
-    if (c == 0)
-    {
-        return ((char *)s);
-    }
-    return(NULL);
+	unsigned int	i;
+	unsigned int	s_len;
+
+	i = 0;
+	s_len = ft_strlen(s) + 1;
+	while (i < s_len)
+	{
+		if (s[i] == (char) c)
+			return ((char *) &s[i]);
+		i++;
+	}
+	return (NULL);
 }
