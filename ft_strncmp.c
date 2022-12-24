24a2ed3926396e6e7e4ba80f026d29618a4b8e0a
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-24 14:06:48 +0100

    ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 13:56:47 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/23 16:26:00 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2022/12/24 13:58:49 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		if (s1[i] < s2[i])
-			return ((unsigned char) s1[i] - s2[i]);
-		else if (s1[i] > s2[i])
-			return ((unsigned char) s1[i] - s2[i]);
-		else if (s1[i] == '\0' || s2[i] == '\0')
-			return (0);
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
