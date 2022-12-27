e2da560484639e33fcde8acbdc5576fe4fe92432
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-27 15:02:33 +0100

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 16:25:46 by jknyzhen          #+#    #+#             */
-/*   Updated: 2022/12/27 14:28:24 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2022/12/27 15:01:05 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,5 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-
+void	*ft_calloc(size_t count, size_t size);
 #endif
