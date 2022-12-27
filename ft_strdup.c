37fd738885ebccd718cb580b203a20f5d8188d37
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-27 17:32:13 +0100

    ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/27 16:41:45 by jknyzhen          #+#    #+#             */
+/*   Updated: 2022/12/27 17:28:08 by jknyzhen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdlib.h>
+//#include <string.h>
+
+//size_t	ft_strlen(const char *s)
+//{
+//	size_t	i;
+//
+//	i = 0;
+//	while (s[i])
+//	{
+//		i++;
+//	}
+//	return (i);
+//}
+
+
+
+char	*ft_strdup(const char *s1)
+{
+	char			*p;
+	unsigned int	s_lenght;
+
+	p = 
+}
