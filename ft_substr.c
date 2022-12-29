b75577598d3c1ec33ad61f85e45a9abb16956fe1
jknyzhen <jknyzhen@student.42wolfsburg.de>
2022-12-29 14:33:28 +0100

    ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/28 15:58:18 by jknyzhen          #+#    #+#             */
+/*   Updated: 2022/12/29 14:33:03 by jknyzhen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <limits.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*substr;
+
+	i = 0;
+	j = 0;
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!s)
+	{
+		return (NULL);
+	}
+	if (!substr)
+	{
+		return (NULL);
+	}
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			substr[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	substr[j] = '\0';
+	return (substr);
+}
+//int	main()
+//{
+//	char	*str;
+//
+//	str = ft_substr("yuliana", 6, 3);
+//	printf("%s\n", str);
+//	return (0);
+//}
