4194aa39ca0db2c0adf6341a78cba5185dc0a69e
jknyzhen <jknyzhen@student.42wolfsburg.de>
2023-01-02 16:05:58 +0100

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 16:25:46 by jknyzhen          #+#    #+#             */
-/*   Updated: 2023/01/02 15:42:19 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2023/01/02 16:05:23 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	**ft_split(char const *s, char c);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 #endif
