b32995d91483dedbe3691b62c0f2a5128d0ef044
jknyzhen <jknyzhen@student.42wolfsburg.de>
2023-01-02 18:10:47 +0100

    ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 15:54:41 by jknyzhen          #+#    #+#             */
-/*   Updated: 2023/01/02 17:32:40 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2023/01/02 18:07:51 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,57 @@
 #include <stdlib.h>
 #include <limits.h>
 
+size_t	get_len(int n)
+{
+	size_t	len;
+
+	if (n == 0)
+		return (1);
+	len = 0;
+	if (n < 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
 {
-	int		num;
-	int		count;
-	char	*str;
+	size_t	len;
+	size_t	i;
+	char	*result;
 
-	num = n;
-	count = 0;
-	while (num != 0)
+	len = get_len(n);
+	if (!(result = (char*)malloc(len + 1)))
+		return (NULL);
+	result[len] = '\0';
+	if (n < 0)
 	{
-		num = num / 10;
-		count++;
+		result[0] = '-';
+		i = 1;
 	}
-	str = (char *)malloc(sizeof(char) * (count + 1 + (n < 0) + (n == 0)));
-	if (!str)
-		return (str);
-	if (num !== 0)
-	{
-		str[0] = '0';
-		str[1] = '\0'
-		return (str);
-	}
-	while (n != 0)
-	{
-	
-	}
-	return((char *)0)
+	else
+		i = 0;
+	while (len-- > i)
+		if (n < 0)
+		{
+			result[len] = '0' + n % 10 * (-1);
+			n = n / 10;
+		}
+		else
+		{
+			result[len] = '0' + n % 10;
+			n = n / 10;
+		}
+	return (result);
 }
+
+// int main()
+// {
+// 	printf("%s\n", ft_itoa(123456789));
+// 	printf("%s\n", ft_itoa(-123456789));
+// 	printf("%s\n", ft_itoa(0));
+// }
