6a1b0ae26714f509fdf93a97d268ca91d7235aef
jknyzhen <jknyzhen@student.42wolfsburg.de>
2023-01-04 18:58:03 +0100

    ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 17:25:58 by jknyzhen          #+#    #+#             */
-/*   Updated: 2023/01/03 17:39:21 by jknyzhen         ###   ########.fr       */
+/*   Updated: 2023/01/04 17:36:45 by jknyzhen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,24 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
+	unsigned int	i;
 
+	if (s && f)
+	{
+		i = 0;
+		while (s[i])
+		{
+			f(i, &s[i]);
+			i++;
+		}
+	}
 }
+
+// int main()
+// {
+// 	char str1[] = "abc";
+// 	char	*str2;
+
+// 	str2 = ft_striteri(str1, *f);
+// 	printf("%s\n", str2);
+// }
