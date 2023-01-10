ed8344ca2dc38d63729cdae751a73da2e33abf5f
jknyzhen <jknyzhen@student.42wolfsburg.de>
2023-01-10 19:06:40 +0100

    ft_lstdelone.c

---
+++ ft_lstdelone.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jknyzhen <jknyzhen@42wolfsburg.de>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/10 18:29:06 by jknyzhen          #+#    #+#             */
+/*   Updated: 2023/01/10 19:01:30 by jknyzhen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if (!lst)
+		return ;
+	(*del)(lst->content);
+	free(lst);
+}
